e6b8aaf6a5c2ad12580de0a2b15fd07024b6b5ee
Yash Raj <yashraj.pratap@gmail.com>
2022-01-12 13:43:07 +0530

    Added Navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;

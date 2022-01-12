29f9b2f9127dc9c4aae3db9f55fb5647ee8fd33a
Yash Raj <yashraj.pratap@gmail.com>
2022-01-12 14:45:53 +0530

    Created Slider Component

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1603302576837-37561b2e2302?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGxhcHRvcHxlbnwwfHwwfHw%3D&w=1000&q=80",
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1610277027770-abb0f444c4e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bWFjYm9vayUyMGFpcnxlbnwwfHwwfHw%3D&w=1000&q=80",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1588702547923-7093a6c3ba33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE",
+    bg: "fbf0f4",
+  },
+];

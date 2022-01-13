4c56973ae02c6cec13fb94091dd96ce6a597e848
Yash Raj <yashraj.pratap@gmail.com>
2022-01-13 11:06:05 +0530

    Added Category Component

--- src/data.js
+++ src/data.js
@@ -15,9 +15,27 @@ export const sliderItems = [
   },
   {
     id: 3,
-    img: "https://images.unsplash.com/photo-1588702547923-7093a6c3ba33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    img: "https://images.unsplash.com/photo-1611078489935-0cb964de46d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
     title: "WINTER SALE",
     desc: "DON'T COMPROMISE",
     bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1588601515608-6a6814976b50?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    title: "LAPTOP",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1587749090881-1ea18126ab3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    title: "MOBILE",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1561154464-82e9adf32764?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    title: "TABLET",
   },
 ];
